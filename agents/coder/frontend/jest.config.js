68deb407c1f2ef22f7b1b89541971c4c9c307883
Liffex <lilian.paul@efrei.net>
2026-04-18 22:25:24 +0200

    oui

--- agents/coder/frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
